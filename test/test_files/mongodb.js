8873e251d0cd7b32491aa156cb5b4d31f77cf6eb
Eddie <edward@moblox.io>
2016-01-29 00:02:34 -0300

    Test: reading 100k from mongodb. Generate xlsx and viceversa

--- test/test_files/mongodb.js
+++ test/test_files/mongodb.js
@@ -1,7 +1,22 @@
-var mongoose = require('mongoose');
+/**
+
+100.000 / 3 element dataset
+i7 @ 2.2 GHZ w/16GB
+
+0.4[s] 'MODEL DONE'
+6.1[s] 'WRITING XLSX DONE'
+4.1[s] 'READING AND PARSING XLSX DONE'
+
+*/
+
 var mongoxlsx = require('../../lib/mongo-xlsx');
+// External
+var mongoose = require('mongoose');
+var async = require('async');
+// System
 var fs = require('fs');
 
+// Config DB
 mongoose.connect('mongodb://localhost/mongo-xlsx-test');
 var db = mongoose.connection;
 
@@ -13,28 +28,51 @@ db.once('open', function() {
 var startTest = function() { 
 
 var simpleSchema = mongoose.Schema({
-    name: String
+    name: String,
+    lastname: String,
+    createdAt: { type: Date, default: Date.now },
+    age: Number
 });
 var SimpleModel = mongoose.model('SimpleTest', simpleSchema);
 
-new SimpleModel({ name : 'abc' }).save(function() {
-  new SimpleModel({ name : 'xyz' }).save(function() {
-    SimpleModel.find().exec(function(err, data) {
+var names = new Array(100000);
 
+async.series([
+  function(next) {
+    SimpleModel.remove({}, next);
+  },
+  function(next) {
+    async.eachLimit(names, 1000, function(name, done) {
+      new SimpleModel({ name : 'abc', lastname : 'abc', age : 25  }).save(done);
+    }, next);
+  }],
+  function(err) {
+
+    SimpleModel.find().exec(function(err, data) {
+      var time = [];
+      time[0] = new Date().getTime()/1000;
+      console.log(0, "START");
       /* Generate automatic model for processing (A manual model may be used!) */
       var model = mongoxlsx.buildDynamicModel(data);
-      console.log(model);
 
+      time[1] = new Date().getTime()/1000;
+      console.log(time[1]-time[0],"MODEL DONE");
       /* Generate data for excel */
       mongoxlsx.mongoData2Xlsx(data, model, function(err, data) {
-        console.log(data);
 
+        time[2] = new Date().getTime()/1000;
+        console.log(time[2]-time[1], "WRITING XLSX DONE");
         /* Generate JSON from excel */
         mongoxlsx.xlsx2MongoData(data.fullPath, model, function(err, json) {
-          console.log(err, json);
+          time[3] = new Date().getTime()/1000;
+          console.log(time[3]-time[2], "READING AND PARSING XLSX DONE");
+
+          console.log(json.length);
+          console.log(json[0], json[json.length-1]);
         });
       });
     });
-  });
-});
+  }
+);
+
 }
